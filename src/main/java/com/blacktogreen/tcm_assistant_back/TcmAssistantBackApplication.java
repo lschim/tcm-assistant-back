2c81b8b33a8da89e89755a4a249801256cac2e6e
Louis Schimchowitsch <schim.louis@gmail.com>
2026-01-20 18:54:29 +0100

    chore: Apply format

--- src/main/java/com/blacktogreen/tcm_assistant_back/TcmAssistantBackApplication.java
+++ src/main/java/com/blacktogreen/tcm_assistant_back/TcmAssistantBackApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TcmAssistantBackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TcmAssistantBackApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TcmAssistantBackApplication.class, args);
+  }
 }
--- src/test/java/com/blacktogreen/tcm_assistant_back/TcmAssistantBackApplicationTests.java
+++ src/test/java/com/blacktogreen/tcm_assistant_back/TcmAssistantBackApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TcmAssistantBackApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }

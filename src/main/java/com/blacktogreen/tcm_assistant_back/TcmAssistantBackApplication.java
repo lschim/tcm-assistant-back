856098c2126bf91bc34cd41d753c3eb1a5797e4d
Louis Schimchowitsch <schim.louis@gmail.com>
2026-01-20 16:21:54 +0100

    chore: Initialize Springboot application

---
+++ src/main/java/com/blacktogreen/tcm_assistant_back/TcmAssistantBackApplication.java
@@ -0,0 +1,13 @@
+package com.blacktogreen.tcm_assistant_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcmAssistantBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcmAssistantBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blacktogreen/tcm_assistant_back/TcmAssistantBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blacktogreen.tcm_assistant_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcmAssistantBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
